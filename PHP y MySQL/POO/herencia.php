6e2d784fc6cd20702734aa41fd6697f9feb335da
FrankCinatra <L18390031@chetumal.tecnm.mx>
2022-03-29 17:59:46 -0500

    Seccion 5 del curso de PHP y MySQL. Fundamentos de Programación Orientada a Objetos en PHP.

---
+++ PHP y MySQL/POO/herencia.php
@@ -0,0 +1,36 @@
+<?php
+    class Persona {
+        public $nombre;
+        public $edad;
+        public $pais;
+
+        function __construct($nombre, $edad, $pais){
+            $this->nombre = $nombre;
+            $this->edad = $edad;
+            $this->pais = $pais;
+        }
+
+        public function mostrarInformacion(){
+            return $this->nombre . ' tiene ' . $this->edad . ' años de edad y es de ' . $this->pais;
+        }
+    }
+
+    class Estudiante extends Persona {
+        function __construct($nombre, $edad, $pais, $carrera){
+            parent::__construct($nombre, $edad, $pais);
+            $this->carrera = $carrera;
+        }
+    }
+
+    class Trabajador extends Persona {
+        function __construct($nombre, $edad, $pais, $trabajo){
+            parent::__construct($nombre, $edad, $pais);
+            $this->trabajo = $trabajo;
+        }
+    }
+
+    $carlos = new Trabajador('Carlos Arturo', 23, 'Mexico', 'Profesor');
+    echo $carlos->mostrarInformacion();
+    echo ' y su Profesión es ' . $carlos->trabajo;
+
+?>
---
+++ PHP y MySQL/POO/static.php
@@ -0,0 +1,17 @@
+<?php
+    class Persona {
+        public static $dia = '7 de Septiembre';
+
+        public static function saludo($nombre = null){
+            if($nombre){
+                return 'Hola, buen dia ' . $nombre;
+            } else {
+                return 'Hola, buen dia.';
+            }
+        }
+    }
+
+    $carlos = new Persona;
+    echo $carlos->saludo('Carlos') . '<br>';
+    //echo Persona::saludo('Carlos');
+?>
