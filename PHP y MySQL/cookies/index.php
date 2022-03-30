ab21efe4438037349d93f28b58d32508b66c1a1b
FrankCinatra <L18390031@chetumal.tecnm.mx>
2022-03-30 12:36:55 -0500

    Seccion 7 del curso de PHP y MySQL. Uso y función de Cookies y Sesiones e PHP.

---
+++ PHP y MySQL/cookies/index.php
@@ -0,0 +1,14 @@
+<?php
+    //                                     seg  min   H   Days  Ruta almacenamiento
+    setcookie('font-size', '30px', time() + 60 * 60 * 24 * 30, '/');
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<h1>Cookie Seteada</h1>
+</body>
+</html>
